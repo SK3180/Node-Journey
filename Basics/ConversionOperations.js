43e2d1cc225568320904836c5ed4e237c1a62431
Sachin Kumar Singh <sachin318031@gmail.com>
2025-01-05 23:57:28 +0530

    ConversionOperations

--- Basics/ConversionOperations.js
+++ Basics/ConversionOperations.js
@@ -27,3 +27,48 @@ console.log(stringNumber)
 console.log(typeof stringNumber)
 
 
+
+// ************************** Operations **************************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2) // 2 raise to the power 2
+
+
+let str1 = "Sk"
+let srt2 = " Singh"
+let str3 = str1 + srt2
+console.log(str3)
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+
+let gameCounter = 10
+//gameCounter++
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
