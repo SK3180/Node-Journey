86926b48a79247f69504a83f518d5432cc77b5c0
Sachin Kumar Singh <sachin318031@gmail.com>
2025-01-06 00:36:08 +0530

    Memories

--- Basics/DataTypes_Summary.js
+++ Basics/DataTypes_Summary.js
@@ -46,3 +46,37 @@ myFunction()
 //        Object  =>  object
 
 // JavaScript is a dynamically typed language. This means that variable types are determined at runtime, and you do not need to explicitly declare the type of a variable before using it. You can assign different types of values to a variable during its lifetime.
+
+
+
+
+// ******************************* Memories ***********************
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+// Stack - Copy of value(make a copy of the value and work on it)
+
+// Heap - Reference of Value (if change value then it reflect in the main value)
+
+
+let myYoutubename = "JustSkThings"
+
+let anotherName = myYoutubename
+anotherName = "JustSkPlayz"
+
+console.log(myYoutubename)
+console.log(anotherName)
+
+let user =  {
+    email: "SK@gmail.com",
+    name : "SkSingh"
+}
+
+let user2 = user
+
+user2.email = "goat@gmail.com"
+user2.name = "GOAT"
+
+
+console.log(user)
+console.log(user2)
