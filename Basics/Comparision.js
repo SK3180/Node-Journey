7199aa345e452a8eaf4ddb66d9a8f5202c3aee62
Sachin Kumar Singh <sachin318031@gmail.com>
2025-01-06 00:08:39 +0530

    Comparision

---
+++ Basics/Comparision.js
@@ -0,0 +1,31 @@
+console.log(1>2)
+let score = 0
+
+console.log(1>=2)
+console.log(1<2)
+console.log(1==2)
+console.log(1!=2)
+
+// Example
+if( score > 1) {
+    console.log("Greater then 1")
+} 
+else
+{
+    console.log("Smaller then 1")
+}
+
+console.log(1 > "2")
+console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+
+console.log(undefined == 0)
+
+
+// === -> strict check
+
+console.log("2" === 2)
+
