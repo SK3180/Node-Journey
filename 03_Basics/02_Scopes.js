84cbc4c6941d99ddae4f60888b9944879596248b
Sachin Kumar Singh <sachin318031@gmail.com>
2025-01-21 14:35:25 +0530

    02_Scopes

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -10,3 +10,45 @@ console.log(a)
 //console.log(b)
 console.log(c)
 
+// nested Scopes
+
+function one(){
+    const username = "Sk"
+    function two(){ 
+    const email = "Sk3180@gmail.com" 
+    console.log(username)
+    }
+   // console.log(email) // email is not accessable in this scope
+
+    two()
+}
+
+// one()
+
+if(true){
+    const userName = "Sachin"
+    if(userName === "Sachin"){
+        const website = " youtube"
+        console.log(userName + website)
+    }
+  //  console.log(website) // showing errors
+}
+// console.log(userName) // showing errors
+
+
+// ******************** Interesting ***********************
+
+
+//console.log(addone(10))
+function addone(num){
+    return num + 1
+}
+console.log(addone(10))
+
+//console.log(addTwo(11))
+
+const addTwo = function(num){  // also known as expression
+    return num + 2
+}
+
+console.log(addTwo(11))
