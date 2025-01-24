a2b9f6ffe780b0b5a1fb94bb6b2b8e3ec0936cbb
Sachin Kumar Singh <sachin318031@gmail.com>
2025-01-24 14:20:08 +0530

    05_JEC.

---
+++ 03_Basics/05_javascript_ec.js
@@ -0,0 +1,8 @@
+// Javascript Execution Context
+
+// {whole code} -> Global Execution Context (Global = this)
+
+// Global Exrcution Context
+// Function Execution Context
+// Eval Execution Context
+
