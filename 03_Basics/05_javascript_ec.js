5d2fa0c3bc9328169c3dbb7291db6301e506989e
Sachin Kumar Singh <sachin318031@gmail.com>
2025-01-25 13:30:18 +0530

    05_JEC&working

--- 03_Basics/05_javascript_ec.js
+++ 03_Basics/05_javascript_ec.js
@@ -6,3 +6,23 @@
 // Function Execution Context
 // Eval Execution Context
 
+
+
+// working  ->
+
+    // -> Global Execution = this
+
+    // -> Memory Phase = val1 -> Undifined , function -> defination
+    
+    // -> Execution phase = val 1 = 10,addNum -> will create new context which contains new variable environment  + Execution thread
+
+        //  Memory Phase  (for function AddNum)
+        // Execution phase (for function AddNum)
+
+
+        // Call Stack : - 
+        // This contains Global Execution at bottom and method comes and perform then goes out of stack after execution just like LIFO(last in first out)
+
+        
+
+
