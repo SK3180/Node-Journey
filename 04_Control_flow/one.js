cd33916015436db8d4c2ca059c818eb98003944c
Sachin Kumar Singh <sachin318031@gmail.com>
2025-01-30 14:35:50 +0530

    Control_Flow

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,31 @@
+// Control Flow
+
+// if 
+
+const isUserLoggedIn = true
+const temperature = 12
+
+if (temperature<50){
+    console.log("Less then 50")
+}
+else
+{
+    console.log("TemperatureMore then 50")
+}
+
+if(2 == "2"){
+    console.log("2 == 2, 1st")
+}
+if(2 === "2"){
+    console.log("2 === 2, 2nd")
+}
+// <, >, <=, >=, ==, !=, === , !==
+
+// === this also check the data type of variable
+
+// !==  this check now equals also datatype
+
+
+
+
+
