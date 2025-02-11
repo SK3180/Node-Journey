bcd70b5f2462e340500dc7acfc2a268e7c7a8126
Sachin Kumar Singh <sachin318031@gmail.com>
2025-02-11 12:25:20 +0530

    basic_loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+let value = 7
+
+while(value<10){
+    console.log("Hemlo")
+    console.log("Sir")
+    value = value+1
+}
+
+let myArray = [ "IronMan","Superman","Batman"]
+let arr = 0
+
+while(arr<myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do{
+    console.log(score)
+    score++
+}while (score <= 5)
+
